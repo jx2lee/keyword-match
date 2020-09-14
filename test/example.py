71f784e155cc77d489469e8facbac6c32edf8939
jx2lee <jaejun.lee.1991@gmail.com>
2020-09-14 13:20:41 +0900

    edited database info

--- test/example.py
+++ test/example.py
@@ -5,13 +5,18 @@ import pandas as pd
 
 if __name__ == "__main__":
     import sys
-    sys.path.append('..//')
-    from keywordmatch import MatchingProcessor
+    import os
 
-    with open("example.pickle", "rb") as f:
+    run_path = os.getcwd()
+    sys.path.append(run_path)
+    from keywordmatch import MatchingProcessor
+    
+
+    with open(f'{run_path}/test/example.pickle', "rb") as f:
         test_df = pickle.load(f)
     test_df_keyword = pd.DataFrame({'타입':['금융', '주택', '금융', '주택', '금융', '주택'],
                                     '키워드':['은행', '중랑구', '송금', '부산', '출금', '경남']})
+
 
     instance = MatchingProcessor(test_df, '기사내용', ['주택', '금융'])
     instance.set_logger('t1', is_file=False)
@@ -19,12 +24,12 @@ if __name__ == "__main__":
     instance.get_keyword_processor(test_df_keyword, '타입', '키워드')
     result = instance.is_keyword()
 
-    instance._data['수집시간'] = datetime(2020, 8, 19).strftime('%Y-%m-%d')
-    tibero_info = {'ip': '192.168.179.166',
-              'port': '8629',
-              'sid': 'tibero',
-              'id_pw': ['tibero', 'tmax'],
+    instance._data['수집시간'] = datetime(2020, 9, 14).strftime('%Y-%m-%d')
+    tibero_info = {'ip': '{database_ip}',
+              'port': '{database_port}}',
+              'sid': '{database_sid}',
+              'id_pw': ['{id}', '{password}'],
               'output_columns': ['기사제목', '기사내용', '주택', '금융', '수집시간'],
-              'table': 'CRAWLER_DATA',
+              'table': '{table_name}',
               'table_columns': ['DETECTED_LINK', 'DETECTED_CONTENTS', 'IS_FINANCE', 'IS_HOUSE', 'DETECTED_TIME']}
-    instance.save_output_database(jar_file='../' + 'lib/' + 'tibero6-jdbc.jar', db_info=tibero_info)
+    instance.save_output_database(jar_file=f'{run_path}/lib/tibero6-jdbc.jar', db_info=tibero_info)
